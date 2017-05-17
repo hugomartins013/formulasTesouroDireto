327c2477d2d714c870323d89ecd9f5ff47168949
Hugo Martins <hugomartins_15@hotmail.com>
2017-05-17 15:23:54 -0300

    Teste do método precoUnitario passando, retornando 0

--- src/formulasTesouroDireto/BonusTesouroNacional.java
+++ src/formulasTesouroDireto/BonusTesouroNacional.java
@@ -2,4 +2,9 @@ package formulasTesouroDireto;
 
 public class BonusTesouroNacional {
 
+	public double precoUnitario() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- test/br/unb/cas/tesouro/TestBonusTesouroNacional.java
+++ test/br/unb/cas/tesouro/TestBonusTesouroNacional.java
@@ -1,12 +1,24 @@
 package br.unb.cas.tesouro;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestFormula {
+import formulasTesouroDireto.BonusTesouroNacional;
 
+public class TestBonusTesouroNacional {
+
+	BonusTesouroNacional valorUnitario;
+	
+	@Before
+	public void setUp() throws Exception {
+		valorUnitario = new BonusTesouroNacional();
+	}
+	
 	@Test
-	public void testCompilar() {
-		
+	public void testValorUnitario() {
+		double precoUnitario = valorUnitario.precoUnitario();
+		assertEquals(0, precoUnitario, 0.0001);
 	}
 		
 }
