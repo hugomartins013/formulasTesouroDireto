446d18bf7caf0002083350045ac10f6575b4bbbf
jessicacris <jessica.cris1127@gmail.com>
2017-05-17 18:10:04 -0300

    Criando teste do NTNB

---
+++ test/br/unb/cas/tesouro/TestNTNB.java
@@ -0,0 +1,24 @@
+package br.unb.cas.tesouro;
+
+import org.junit.Test;
+
+public class TestNTNB {
+	
+	@Test
+	public void testValorNominalVNA() {
+		
+	}
+	
+	@Test
+	public void testFator(){
+		
+	}
+	
+	@Test
+	public void testJuros(){
+		
+	}
+	
+	
+
+}
