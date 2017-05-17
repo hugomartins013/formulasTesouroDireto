6af1f69776b22f70a664136fc8abf3e5f9410e1b
jessicacris <jessica.cris1127@gmail.com>
2017-05-17 17:59:40 -0300

    Criando implementação e refatoração dos testes LFTA

--- test/br/unb/cas/tesouro/TestLFTA.java
+++ test/br/unb/cas/tesouro/TestLFTA.java
@@ -1,14 +1,25 @@
 package br.unb.cas.tesouro;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 
 public class TestLFTA {
+
+	private LFTA lfta;
 	
+	@Before
+	public void init(){
+		lfta = new LFTA();
+	}
 	
 	@Test
 	public void testValorNominalVNA() {
 		
-
+		double resultado = lfta.calculaVNA(2,2);
+		assertEquals(resultado, -2.0004879999930996d, 0.0001d);
+		
 	}
 		
 }
