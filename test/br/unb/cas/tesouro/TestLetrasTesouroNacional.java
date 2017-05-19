6c0b20a42ebf204f170c0f189fc1842d43545a4d
Hugo Martins <hugomartins_15@hotmail.com>
2017-05-19 11:08:53 -0300

    Testes do método precoUnitarioLTN

---
+++ test/br/unb/cas/tesouro/TestLetrasTesouroNacional.java
@@ -0,0 +1,31 @@
+package br.unb.cas.tesouro;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import formulasTesouroDireto.LetrasTesouroNacional;
+
+public class TestLetrasTesouroNacional {
+	
+	private LetrasTesouroNacional precoUnitario;
+	public static double FORMATER_6 = 0.000001d;
+	
+	@Before
+	public void setUp() throws Exception {
+		precoUnitario = new LetrasTesouroNacional();
+	}
+
+	@Test
+	public void testEqualsPrecoUNitario() {
+		double precoUnitarioLTN = precoUnitario.precoUnitarioLTN(10.22);
+		assertEquals(990.451836, precoUnitarioLTN, FORMATER_6);
+	}
+	
+	@Test
+	public void testNotEqualsPrecoUNitario() {
+		double precoUnitarioLTN = precoUnitario.precoUnitarioLTN(10.22);
+		assertNotEquals(11, precoUnitarioLTN, FORMATER_6);
+	}
+
+}
