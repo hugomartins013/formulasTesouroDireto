78323a000a9a5eeae01db9e12d73e3938b3573c0
Hugo Martins <hugomartins_15@hotmail.com>
2017-05-22 14:46:41 -0300

    Criando função  dos métodos jurosA6, ValorNominalAtualizadoA6 e fatorJurosA6

--- src/formulasTesouroDireto/NotasTesouroNacional.java
+++ src/formulasTesouroDireto/NotasTesouroNacional.java
@@ -53,4 +53,18 @@ public class NotasTesouroNacional {
 		return jurosVNA;
 	}
 
+	public double fatorJurosVNA(int taxaJuros, double mesJuros) {
+		fatorJurosVNA = ((taxaJuros/100.0)*(mesJuros/12.0));
+		return fatorJurosVNA;
+	}
+
+	public double valorNominalAtualizadoA6(double USSa, double USSb, double valorEmissao) {
+		valorNominalAtualizadoA6 = (USSa/USSb)*valorEmissao;
+		return valorNominalAtualizadoA6;
+	}
+
+	public double jurosA6(double fatorJurosNTN, double valorNominalAtualizadoA6) {
+		jurosA6 = fatorJurosNTN * valorNominalAtualizadoA6;
+		return jurosA6;
+	}
 }
