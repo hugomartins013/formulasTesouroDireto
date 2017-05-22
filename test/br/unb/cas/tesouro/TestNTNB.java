80729cefd80c6b25f3b24e4b700c357b465ac9af
Jéssica Cristina <jessica.cris1127@gmail.com>
2017-05-22 16:16:54 -0300

    Aplicando triangulação no teste NTNB

--- test/br/unb/cas/tesouro/TestNTNB.java
+++ test/br/unb/cas/tesouro/TestNTNB.java
@@ -26,10 +26,27 @@ public class TestNTNB {
 	}
 	
 	@Test
+	public void testNotValorNominalVNA() {
+		double resultado = ntnb.calculaVNA();
+		assertNotEquals(resultado, 5000d, FORMATER_6);
+		double resultadoTest5 = ntnb.calculaVNA();
+		assertNotEquals(resultadoTest5, 24556d, FORMATER_6);
+		
+	}
+	
+	@Test
 	public void testFator(){
 			double resultado = ntnb.calculaFator();
 			assertEquals(resultado, 245d, FORMATER_6);
 		
+	}
+	
+	public void testNotFator(){
+		double resultado = ntnb.calculaFator();
+		assertNotEquals(resultado, 6000d, FORMATER_6);
+		double resultadoTest7 = ntnb.calculaFator();
+		assertNotEquals(resultadoTest7, 24545d, FORMATER_6);
+	
 	}
 	
 	@Test
@@ -39,9 +56,25 @@ public class TestNTNB {
 	}
 	
 	@Test
+	public void testNotJuros(){
+		double resultado = ntnb.calculaJuros();
+		assertNotEquals(resultado, 50867d, FORMATER_6);
+		double resultadoTest7 = ntnb.calculaJuros();
+		assertNotEquals(resultadoTest7, 59870d, FORMATER_6);
+	}
+	
+	@Test
 	public void testFatorJuros(){
 		double resultado = ntnb.calculaFatorJuros();
 		assertEquals(resultado, FORMATER_5, FORMATER_6);
+	}
+	
+	@Test
+	public void testNotFatorJuros(){
+		double resultado = ntnb.calculaFatorJuros();
+		assertNotEquals(resultado, 80000, FORMATER_6);
+		double resultadoTest8 = ntnb.calculaFatorJuros();
+		assertNotEquals(resultadoTest8, 1.9194273d, FORMATER_6);
 	}
 	
 	
