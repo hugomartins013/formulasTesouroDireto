12f382efd03549ec55c88d9a31d1cd1d40d36bb6
Jéssica Cristina <jessica.cris1127@gmail.com>
2017-05-22 16:05:21 -0300

    Aplicando triangulação no test LFT

--- test/br/unb/cas/tesouro/TestLFT.java
+++ test/br/unb/cas/tesouro/TestLFT.java
@@ -28,11 +28,30 @@ public class TestLFT {
 	}
 	
 	@Test
+	public void testNotValorNominalVNA() {
+		
+		double resultado = lft.calculaVNA(2,2);
+		assertNotEquals(resultado, 5d, FORMATER_6);
+		double resultadoTest1 = lft.calculaVNA(2,2);
+		assertNotEquals(resultadoTest1, 10d, FORMATER_6);
+	}
+	
+	@Test
 	public void testValorNominalC(){
 		
 		double selic = 1.11d;
 		double resultado = lft.calculaC(selic);
 		assertEquals(resultado, FORMATER_8, FORMATER_6);
+	}
+	
+	@Test
+	public void testNotValorNominalC(){
+		
+		double selic = 1.11d;
+		double resultado = lft.calculaC(selic);
+		assertNotEquals(resultado, 10000, FORMATER_6);
+		double resultadoTest3 = lft.calculaC(selic);
+		assertNotEquals(resultadoTest3, 2.11000563d, FORMATER_6);
 	}
 	
 	@Test
@@ -42,6 +61,16 @@ public class TestLFT {
 		double resultado = lft.calculaTSelic(selick);
 		
 		assertEquals(resultado, FORMATER_6, 0.0001d);
+	}
+	
+	@Test
+	public void testNotValorNominalTselic(){
+		
+		double selick = 2.22d;
+		double resultado = lft.calculaTSelic(selick);
+		assertNotEquals(resultado, 10001, 0.0001d);
+		double resultadoTest4 = lft.calculaTSelic(selick);
+		assertNotEquals(resultadoTest4, 0.000326d, 0.0001d);
 	}
 		
 }
